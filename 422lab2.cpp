7dce837658a95ff20703ba74e731ae7c430daa94
huluhululiu <jiangnanliu0530@gmail.com>
2017-10-28 14:18:25 -0500

    first ct

--- 422lab2.cpp
+++ 422lab2.cpp
@@ -10,8 +10,14 @@
 using namespace std;
 int main(int argc,char* argv[])
 {
+
 	try {
-		start(argc, argv);
+		vector<int> numbers;
+		start(argc, argv,numbers);
+		cout<<"vector numbers:"<<endl;
+		for (auto i = numbers.begin(); i != numbers.end(); ++i){
+			std::cout << *i << ' ';
+		}
 		//if none of the game could be start, it means the command is wrong 
 	}
 	// print out the bad alloc with usage message or any other results code and its corresponding error message
@@ -19,7 +25,9 @@ int main(int argc,char* argv[])
 		return usage(argv[program_name], a);
 	}
 }
-int start(int argc, char* argv[]){
+int start(int argc, char* argv[], vector<int>& num){
+	int roundnum;
+	int iterationnum;
 	if (argc < argnum) {
 			throw toofewargc;
 		}
@@ -30,14 +38,34 @@ int start(int argc, char* argv[]){
 		else if ((string(argv[wait_strategy]) != "spin") &&(string(argv[wait_strategy]) != "sleep")) {
 			throw waitstrategy_notcorrect;
 		}
-		else if ((int(argv[rounds]) <= 0)){
+		else if (!checkinput(argv[rounds])){
 			throw rounds_type;
 		}
-		else if ((int(argv[iterations]) <= 0)){
+		else if (!checkinput(argv[iterations])){
 			throw iteration_type;
 		}
-		else return 0;
+		else{ 
+			for (int i=number_times;i<argc;++i){
+				if(!checkinput(argv[i])) throw num_type;
+				else {
+					istringstream ss(argv[i]);
+					int x;
+					ss>>x;
+					num.push_back(x);
+				}
+			}
+			return 0;
+		}
 }
+bool checkinput(char* a){
+	istringstream ss(a);
+	int x;
+	if(!(ss>>x)) return false;
+	ss>>x;
+	if(x<=0) return false; 
+	else return true;
+}
+
 int usage(char * program_name, int error_value)// print out the program's usage when argument number is false or other error happens
 {
 	if (error_value == scheduler_notcorrect) {
--- lab2.h
+++ lab2.h
@@ -1,15 +1,16 @@
 #ifndef LAB2_H
 #define LAB2_H
 
-
+#include <vector>
 
 
 enum ins { program_name = 0, scheduler = 1, wait_strategy = 2, rounds = 3, iterations = 4, number_times=5, };
 enum scheduler_nameSelection { SCHED_RR, SCHED_FIFO, SCHED_NORMAL,};
 enum wait_strategies{spin,sleep,};
 enum results_code {
-	success, scheduler_notcorrect, waitstrategy_notcorrect, rounds_type = 5, argnum=6, toofewargc,unknown_exception = -1, iteration_type = -3,
+	success, scheduler_notcorrect, waitstrategy_notcorrect, rounds_type = 5, argnum=6, num_type, toofewargc,unknown_exception = -1, iteration_type = -3,
 };
-int start(int argc, char* argv[]);
+int start(int argc, char* argv[], std::vector<int>& num);
 int usage(char * program_name, int error_value);
+bool checkinput(char* a);
 #endif
